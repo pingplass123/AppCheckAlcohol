287e76da331393ba37f3263d25e31ac318da18b7
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2020-10-25 23:57:45 +0700

    update new page

---
+++ CheckAL/ResultCalPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CheckAL
+{
+    public partial class ResultCalPage : ContentPage
+    {
+        public ResultCalPage(string HeightCal,string WeightCal,string Cal)
+        {
+            InitializeComponent();
+        }
+    }
+}
