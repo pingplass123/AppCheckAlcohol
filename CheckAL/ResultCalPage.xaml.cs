4abc35010f8f4cf7fafa4d148317d3fcb55364d9
khwmri <72812519+khwmri@users.noreply.github.com>
2020-10-28 22:08:50 +0700

    Update time to burn out of calories.
    Update function that indicate the time to burn out of calories from drinking alcohol beverage.
    Update condition of weight for calculate the time to burn out calories.

--- CheckAL/ResultCalPage.xaml.cs
+++ CheckAL/ResultCalPage.xaml.cs
@@ -8,7 +8,7 @@ namespace CheckAL
     public partial class ResultCalPage : ContentPage
     {
         public double numCal = 0.0;
-        
+        public double numTime = 0.0;
         public ResultCalPage(string WeightCal,string Cal,String al)
         {
             
@@ -18,11 +18,14 @@ namespace CheckAL
             double CalNum = Convert.ToDouble(Cal);
             Calculate(WeightNum,Calnum,al);
             
-
+            timeburn(WeightNum);
+            float timeToBurn = (float)System.Math.Round(numTime, 2);
            
 
             calo.Text = "Calories = " + numCal;
-            time.Text = "Time to burn out Cal = 10 hr";
+            time.Text = "Time to burn out Cal = " + timeToBurn + " m/hr";
+            
+   
         }
          public void Calculate(double w, double c, string type)
         {
@@ -69,6 +72,31 @@ namespace CheckAL
                     numCal = calorie;
                 }
            
+        }
+        public void timeburn(double weightCal) 
+        {
+            double timeCal = 0.0;
+
+            if (weightCal <= 57)
+            {
+                timeCal = (10 / 495) * numCal;
+                numTime = timeCal;
+
+            }
+            else if (weightCal <= 70)
+            {
+
+                timeCal = (10.0 / 614) * numCal;
+                numTime = timeCal;
+
+
+            }
+            else if (weightCal <= 84)
+            {
+                timeCal = (10.0 / 733) * numCal;
+                numTime = timeCal;
+
+            }
         }
         
     }
