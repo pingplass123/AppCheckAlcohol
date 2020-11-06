7abdcb9642fc181a9f38dbaf246525c3858de828
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2020-11-06 20:16:25 +0700

    Update HeartRate page
    update heart rate open flash device for check heart rate

---
+++ CheckAL/HartRatePage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+
+using Xamarin.Forms;
+
+namespace CheckAL
+{
+    public partial class HartRatePage : ContentPage
+    {
+        public HartRatePage()
+        {
+            
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Turn On
+                await Flashlight.TurnOnAsync();
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Turn On
+                await Flashlight.TurnOffAsync();
+
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+
+        }
+    }
+}
