4e8c593efe4675933129563ff2d757b7eba1f7dd
pingplass123 <60729252+pingplass123@users.noreply.github.com>
2020-11-15 00:38:17 +0700

    update model database
    database

---
+++ CheckAL/Model/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+namespace CheckAL.Model
+{
+    public class Singleton
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        public string BAC { get; set; }
+        public string typeAL { get; set; }
+        public string LevelBAC { get; set; }
+
+        public override string ToString()
+        {
+            return this.typeAL + "(" + BAC + " " + LevelBAC +  ")";  
+        }
+    }
+
+    
+}
